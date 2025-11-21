c0159e275707b559a73577b56925b3fa3c6b84d2
Nadia <lamatanzaclasificados@gmail.com>
2025-11-21 19:30:31 -0300

    Corregimos scripts de mas

--- script.js
+++ script.js
@@ -1,67 +1,37 @@
-//NO $(document).ready(function () {
-//jQuery(document).ready(function ($) {    
-    // $('.portfolio-item').isotope({
-    //  	itemSelector: '.item',
-    //  	layoutMode: 'fitRows'
-    //  });
-    // $('.portfolio-menu ul li').click(function () {
-    //     $('.portfolio-menu ul li').removeClass('active');
-    //     $(this).addClass('active');
 
-    //     var selector = $(this).attr('data-filter');
-    //     $('.portfolio-item').isotope({
-    //         filter: selector
-    //     });
-    //     return false;
-    // })
-    // COMENTARIO $(document).ready(function () {
-//     jQuery(document).ready(function ($) {       
-//         var popup_btn = $('.popup-btn');
-//         popup_btn.magnificPopup({
-//             type: 'image',
-//             gallery: {
-//                 enabled: true
-//             }
-//         });
-//     });
-// });
-
-
-//PRUEBA 
-
+/**/
 jQuery(document).ready(function ($) {
-// Initialize Isotope
-var $portfolioItems = $('.portfolio-item');
-$portfolioItems.isotope({
-    itemSelector: '.item',
-    layoutMode: 'fitRows'
-});
 
-// Handle the menu click to filter items
-$('.portfolio-menu ul li').click(function () {
-    // Add active class to the clicked item
-    $('.portfolio-menu ul li').removeClass('active');
-    $(this).addClass('active');
+    // 1. Inicializar Isotope después de que carguen las imágenes
+    var $portfolioItems = $('.portfolio-item');
 
-    // Get the filter selector from data-filter attribute
-    var selector = $(this).attr('data-filter');
-
-    // Filter the items based on the selected category
-    $portfolioItems.isotope({
-        filter: selector
+    var $grid = $portfolioItems.isotope({
+        itemSelector: '.item',
+        layoutMode: 'fitRows'
     });
 
-    return false; // Prevent default behavior
-});
+    // Reacomodar cuando cargan las imágenes (soluciona el encimado)
+    $grid.imagesLoaded().progress(function () {
+        $grid.isotope('layout');
+    });
 
-jQuery(document).ready(function ($) {       
-    var popup_btn = $('.popup-btn');
-    popup_btn.magnificPopup({
+    // 2. FILTRO del menú
+    $('.portfolio-menu ul li').click(function () {
+        $('.portfolio-menu ul li').removeClass('active');
+        $(this).addClass('active');
+
+        var selector = $(this).attr('data-filter');
+        $grid.isotope({ filter: selector });
+
+        return false;
+    });
+
+    // 3. Magnific Popup inicializado correctamente
+    $('.popup-btn').magnificPopup({
         type: 'image',
         gallery: {
             enabled: true
         }
     });
-}); 
 
 });
